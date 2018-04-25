f49bc1926971a11a115bc3d9ebbe64001236098e
cjrpriest <chris.priest@me.com>
2018-04-25 07:26:19 +0100

    tidied up and removed unused code and irrelevant comments

--- test/ServerlessMapReduceDotNet.Tests/Extensions/CommandDispatcherMock/CommandDispatcherMockRegistrationExtensions.cs
+++ test/ServerlessMapReduceDotNet.Tests/Extensions/CommandDispatcherMock/CommandDispatcherMockRegistrationExtensions.cs
@@ -33,64 +33,12 @@ namespace ServerlessMapReduceDotNet.Tests.Extensions.CommandDispatcherMock
             return commandDispatcherMock;
         }
         
-        public static ICommandDispatcher Register<
-            TCommandHandler
-        >(this ICommandDispatcher commandDispatcherMock, TCommandHandler commandHandler)
-        where TCommandHandler : ICommandHandler
+        public static ICommandDispatcher Register<TCommandHandler>(this ICommandDispatcher commandDispatcherMock, TCommandHandler commandHandler) where TCommandHandler : ICommandHandler
         {
-/*
-      This code executes the equivalent of this...
-
-            commandDispatcher
-                .DispatchAsync(Arg.Any<TCommand>())
-                .Returns(ci =>
-                    new CommandResult<TResult>(
-                        commandHandlerFactory()
-                            .ExecuteAsync(ci.Arg<TCommand>(), default(TResult))
-                            .Result,
-                        false
-                    )
-                );
-            return commandDispatcher;
-      
-      or this...
-      
-            commandDispatcherMock
-                .DispatchAsync(Arg.Any<StoreObjectCommand>())
-                .Returns(ci => 
-                    {
-                        new MemoryStoreObjectCommandHandler(timeMock, memoryObjectStoreData)
-                            .ExecuteAsync(ci.Arg<StoreObjectCommand>())
-                            .Wait();
-                        return new CommandResult(false);
-                    });
-            return commandDispatcher;
-
-      without knowing TCommand or TResult in advance
-*/
             var registerCommandHandlerExpression = new RegisterCommandHandlerExpressionBuilder2().Build<TCommandHandler>();
             registerCommandHandlerExpression(commandDispatcherMock, commandHandler);
             
-//            var registerCommandHandler = registerCommandHandlerExpression.Compile();
-
-  //          registerCommandHandler(commandDispatcherMock, commandHandlerFactory);
-            
             return commandDispatcherMock;
-        }
-
-        private static void DoIt<TCommand, TResult, TCommandHandler>(ICommandDispatcher commandDispatcher, Func<TCommandHandler> commandHandlerFactory)
-            where TCommand : ICommand<TResult>
-            where TCommandHandler : ICommandHandler<TCommand, TResult>
-        {
-            commandDispatcher
-                .DispatchAsync(Arg.Any<TCommand>())
-                .Returns(async ci =>
-                    new CommandResult<TResult>(
-                        await commandHandlerFactory()
-                            .ExecuteAsync(ci.Arg<TCommand>(), default(TResult)),
-                        false
-                    )
-                );
         }
         
         public static ConfiguredCall ReturnsCommandResult<T>(this Task<CommandResult<T>> value, T returnThis)
@@ -102,6 +50,5 @@ namespace ServerlessMapReduceDotNet.Tests.Extensions.CommandDispatcherMock
         {
             return value.Returns(ci => new CommandResult<T>(returnThis(), false));
         }
-       
     }
 }
