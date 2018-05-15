987fc21a4334015f0b61b1e5b788092e0935a141
cjrpriest <chris.priest@me.com>
2018-05-15 19:26:01 +0100

    remove commented out code and unused directives

--- src/ServerlessMapReduceDotNet/Functions/Mapper.cs
+++ src/ServerlessMapReduceDotNet/Functions/Mapper.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AzureFromTheTrenches.Commanding.Abstractions;
-using Newtonsoft.Json;
 using ServerlessMapReduceDotNet.Abstractions;
 using ServerlessMapReduceDotNet.Commands;
 using ServerlessMapReduceDotNet.Commands.ObjectStore;
-using ServerlessMapReduceDotNet.Model;
 
 namespace ServerlessMapReduceDotNet.Functions
 {    
@@ -66,45 +63,8 @@ namespace ServerlessMapReduceDotNet.Functions
                         IngestedQueueMessageId = ingestedQueueMessage.MessageId
                     });
                 }
-                
-//                using (var memoryStream = new MemoryStream())
-//                using (var streamWriter = new StreamWriter(memoryStream))
-//                using (var streamReader = new StreamReader(ingestedObjectStream))
-//                {
-//                    while (!streamReader.EndOfStream)
-//                    {
-//                        var line = await streamReader.ReadLineAsync();
-//                        
-//                        try
-//                        {
-//                            KeyValuePairCollection keyValuePairs = await _commandDispatcher.DispatchAsync(new MapperFuncCommand {Line = line});
-//                            if (keyValuePairs.Count > 0)
-//                            {
-//                                var keyValuePairsJson = JsonConvert.SerializeObject(keyValuePairs, Formatting.None,
-//                                    new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Auto});
-//                                await streamWriter.WriteLineAsync(keyValuePairsJson);
-//                            }
-//                        }
-//                        catch (Exception e)
-//                        {
-//                            Console.WriteLine($"Error while processing line \"{line}\"");
-//                            Console.WriteLine(e.Demystify());
-//                        }
-//                    }
-//
-//                    await streamWriter.FlushAsync();
-//                    var mapperOutputKey = $"{_config.MappedFolder}/{ingestedDataObjectName}";
-//                    await _commandDispatcher.DispatchAsync(new StoreObjectCommand
-//                    {
-//                        Key = mapperOutputKey,
-//                        DataStream = memoryStream
-//                    });
-//                    await _queueClient.Enqueue(_config.MappedQueueName, mapperOutputKey);
-//
-//                    await _queueClient.MessageProcessed(_config.IngestedQueueName, ingestedQueueMessage.MessageId);
-//                }
             }
-
+            
             await _workerRecordStoreService.RecordHasTerminated("mapper", instanceWorkerId);
             Console.WriteLine($"Mapper {instanceWorkerId} Terminated");
         }
