cf529fcd65c82254e24332c6c0175ee70c5316f7
cjrpriest <chris.priest@me.com>
2018-05-17 06:55:40 +0100

    script for tidying up local object store

---
+++ purgeLocal.sh
@@ -0,0 +1,5 @@
+rm -f ~/FileObjectStore/ingested/*
+rm -f ~/FileObjectStore/mapped/*
+rm -f ~/FileObjectStore/reduced/*
+rm -Rf ~/FileObjectStore/workerRecord/*
+rm -f ~/FileObjectStore/finalReduction/*
